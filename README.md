39fe0c5fd03f66481a48724cfc28e425a002efc8
chencheng <sorrycc@gmail.com>
2015-02-12 12:08:12 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# npm-github
+
+Open url in browser by npm pkg name.
+
+## Install
+
+```bash
+$ npm i npm-github -g
+```
+
+## Usage
+
+```bash
+$ npm-github npm-github
+```
+
+## LICENCE
+
+Copyright (c) 2015 chencheng. Licensed under the MIT license.
---
+++ cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var request = require('request');
+var open = require('open');
+
+var pkg = process.argv[2];
+if (!pkg) {
+  console.error('miss:', 'pkg is missing');
+  process.exit(1);
+}
+
+// get package
+var url = 'http://registry.npmjs.org/' + pkg + '/latest';
+console.info('request:', url);
+
+request(url, function(e, res, body) {
+  var json = JSON.parse(body);
+  var repo = json.repository.url;
+  console.info('repo found:', repo);
+  open(repo);
+});
+
